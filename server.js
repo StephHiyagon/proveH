5da745fc6e3687c0cb9c6c2eb7d551a6fd7a718a
Stephanie Hiyagon <mv.hiyagon@gmail.com>
2017-07-07 08:02:47 -0500

    port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
-app.listen(5000, ()=>{
-  console.log('escucha puerto 5000');
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 })
